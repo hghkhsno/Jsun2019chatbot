4aa0c7de9e5571b48db48f842abcb86241c80cbb
chin-cheng <hghkhsno@gmail.com>
2019-11-22 15:22:16 +0800

    second update

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=coijusticeforcsharp-Web-Deploy.pubxml -p:Password=WB7sp5CwPWs4sF2cA6fZejy9Aa11wlitxfuqH7KzDhrlPlNDwJdzxxCvmeR5
+
