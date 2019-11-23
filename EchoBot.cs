15e65edad22256b97ad867ebcc06905a3c6d5ce5
CHIN-CHENG <hghkhsno@gmail.com>
2019-11-24 00:53:22 +0800

    test20191124V4

--- EchoBot.cs
+++ EchoBot.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using System.Collections.Generic;
@@ -13,7 +13,7 @@ namespace Microsoft.BotBuilderSamples.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var replyText = $"Your {turnContext.Activity.Text} website is https://tw.stock.yahoo.com/q/bc?s={turnContext.Activity.Text}";
+            var replyText = $"\u您的股票 {turnContext.Activity.Text} \u的網址是 https://tw.stock.yahoo.com/q/bc?s={turnContext.Activity.Text}";
             await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
         }
 
